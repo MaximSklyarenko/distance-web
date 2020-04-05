3f883f8a88d3afea46be83b74f2e7c89e38ae3ae
MaximSklyarenko <63196918+MaximSklyarenko@users.noreply.github.com>
2020-04-06 01:51:59 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-﻿cssVars({ 
-        
-    });
+﻿cssVars({
+  
+  // Treat all browsers as legacy
+  onlyLegacy: false,
+  
+  // DEMO: Toggles to see results
+  // ----------------------------
+  // preserveStatic: false,
+  // preserveVars: true,
+  // updateURLs: false,
+  // variables: { '--color': 'purple' },
+  // ----------------------------
+  
+  // Display transformed CSS
+  onComplete: function(cssText, styleNodes, cssVariables, benchmark) {
+    console.log("trash")
+	console.log(cssText);
+  }
+});
+
 
 function addInfo() {
   var panels = document.getElementsByClassName("panel");
